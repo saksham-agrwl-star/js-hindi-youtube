974c80b278ba143ac1806a6ba28c4d858c0f5933
saksham-agrwl-star <saksham06agarwal@gmail.com>
2025-08-21 11:39:00 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,7 +33,7 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
-// console.log(2+2);
+// console.log(2+2); 
 // console.log(2-2);
 // console.log(2*2);
 // console.log(2**3);
@@ -46,10 +46,10 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
 
 // console.log( (3 + 4) * 5 % 3);
 
