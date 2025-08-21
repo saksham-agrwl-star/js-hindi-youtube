0fafdb3610c6b2a4eeb64a6f465cfa027c76a4aa
saksham-agrwl-star <saksham06agarwal@gmail.com>
2025-08-21 12:08:32 +0530

    added answers as comments

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,16 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0);//false
+console.log(null >= 0); //true  
+//this is bcoz comparisons convert null to number treating it as 0
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);// false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); //false
+console.log("2" == 2); //true
