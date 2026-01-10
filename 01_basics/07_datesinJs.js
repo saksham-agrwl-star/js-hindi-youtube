f04dc8bd60dceb9c73c1017cee0fce192659d639
saksham-agrwl-star <saksham06agarwal@gmail.com>
2026-01-10 13:49:59 +0530

    added comments

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -2,15 +2,16 @@
 
 let myDate = new Date()
 // console.log(myDate.toString());
-// console.log(myDate.toDateString());
+//console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
 //let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-11-2023") //month-date-year
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)  //year-month-date-hour-minute
+//let myCreatedDate = new Date("2023-01-14") //here  month starts from 01(yyyy-mm--dd)
+let myCreatedDate = new Date("01-11-2023") //month-date-year(mm-dd-yyyy)
 //console.log(myCreatedDate.toLocaleString());
+//months start from 0 in JS
 
 let myTimeStamp = Date.now()
 
@@ -22,11 +23,14 @@ let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
+console.log(newDate.getHours());
+
 
 // `${newDate.getDay()} and the time `
+//press ctrl + space to get avaliable properties inside localeString
 
 newDate.toLocaleString('default', {
-    weekday: "long",
+    weekday: "long"
     
 })
 
